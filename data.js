4cd62fc7e9f50f9b51abb331e8d03c0de46a32aa
EliasSchramm <privat@eps-dev.de>
2020-07-08 13:42:40 +0200

    Update

---
+++ data.js
@@ -0,0 +1,57 @@
+fs = require('fs');
+
+module.exports = {    
+    loadAllNotes: function (){
+        
+        n = {};        
+         fs.readdir("notes/", (err, files) => {
+            //handling error
+            if (err) {
+                return console.log('Unable to scan directory: ' + err);
+            } 
+            
+            //listing all files using forEach
+            files.forEach((file) => {
+                
+                fs.readFile('notes/' + file, 'utf8', (err,data) => {
+                    console.log(4);
+                    if (err) {
+                      return console.log(err);
+                    }
+                    var raw = data.split(">;$;<");
+                    
+                    if(n[raw[0]] == undefined){                
+                        
+                        n[raw[0]] = [];
+                        n[raw[0]] = [raw[1]];                       
+                        
+                    }else{
+                        n[raw[0]].push(raw[1]);
+                    }                    
+                    
+                  });                  
+            });
+        });
+        
+        return n;
+    },
+
+    saveNote: function (user,note){
+
+        var raw = "";        
+        
+        for(var i = 2; i < note.length; i++){
+            raw += note[i] + " ";            
+        }
+
+        fs.writeFile("notes/" + Date.now() +".txt", user + ">;$;<" + raw, function (err) {
+            if (err) throw err;
+            console.log('File is created successfully.'); 
+        });
+
+        return raw;
+        
+    }
+
+} 
+
--- index.js
+++ index.js
@@ -1,15 +1,54 @@
 const Discord = require('discord.js');
+const { saveNote, loadAllNotes } = require('./data');
 const bot = new Discord.Client();
+const data = new require('./data');
 
 const token = 'NzMwMTAwNzcwODkyMDIxNzgw.XwSlhA.DEd7saZuZ7CJQbTFXd4TVrj_1HE';
 
+const PREFIX = ">";
+
+var notes = loadAllNotes();
+
 bot.on('ready', () =>{
     console.log('This bot is online')
+    console.log(notes);
 })
 
 bot.on('message', msg=>{
-    if(msg.content === "Hello"){
-        msg.reply('Baka');
+    var args = msg.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'n':
+        case 'note':
+            switch(args[1]){
+                case 'a':
+                case 'add':
+                    var raw = saveNote(msg.author.tag, args);
+                    notes[msg.author.tag].push(raw);
+                    msg.reply("Added the new note!");
+                    break;
+                case 's':
+                case 'show':
+                    
+                    if(notes[msg.author.tag]){
+                        for(var i = 0; i < notes[msg.author.tag].length; i++){
+                            msg.reply(i + ":  " + notes[msg.author.tag][i]);
+                        }                           
+                        
+                    }else{
+                        msg.reply("You do not have any notes open.");
+                    }
+
+                    break;
+
+                case 'r':
+                case 'remove':
+                    notes[msg.author.tag].splice(args[2], 1);
+                    msg.reply("Removed note NR." + args[2]);
+                    
+            }    
+            
+        
     }
 })
 
